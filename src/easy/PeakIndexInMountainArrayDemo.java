2407c01358a1f1f5b48f2027b997017681ee0e83
Snidelo <550436269@qq.com>
2019-09-18 12:05:09 +0800

    new solution for PeakIndex..

--- src/easy/PeakIndexInMountainArrayDemo.java
+++ src/easy/PeakIndexInMountainArrayDemo.java
@@ -36,4 +36,17 @@ public class PeakIndexInMountainArrayDemo {
         return -1;
     }
 
+    //=================solution_3================
+    public int peakIndexInMountainArray_3(int[] A){
+        int lo = 0, hi = A.length - 1;
+        while (lo < hi) {
+            int mi = lo + (hi - lo) / 2;
+            if (A[mi] < A[mi + 1])
+                lo = mi + 1;
+            else
+                hi = mi;
+        }
+        return lo;
+    }
+
 }
