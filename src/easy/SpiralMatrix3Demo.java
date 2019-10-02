f54063c59968a23eed0de636da793a34492860c5
Snidelo_T490 <huazefeng95@gmail.com>
2019-10-02 21:19:27 +0800

    slution for Spiral Matrix 3

---
+++ src/easy/SpiralMatrix3Demo.java
@@ -0,0 +1,144 @@
+package easy;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class SpiralMatrix3Demo {
+    public static void main(String[] args) {
+        //7,9,4,1;
+        // 5,6,4,0
+        int[][] arr = new SpiralMatrix3Demo().spiralMatrixIII(7,9,4,1);
+        Arrays.toString(arr);
+    }
+
+    //======solution_1==========
+    //经过仔细思考,这种方法比较麻烦,因为在边界的时候有多种情况
+    /*public int[][] spiralMatrixIII(int R, int C, int r0, int c0) {
+        //其实就是一个走迷宫问题,走过的地方变成墙,直到遍历完整个矩阵
+        ArrayList<Integer> list = new ArrayList<>();
+        int sum = R * C - 1;
+        list.add(r0, c0);
+        while(sum != 0){
+            //先向左走
+            while(true){
+                //在最后一个元素了,直接跳出,去向下走
+                if(c0 == C - 1){
+
+                }
+                break;
+            }
+        }
+    }*/
+
+    //=========solution_2===========
+    //再有一种方法就是,补全整个矩阵,使得遍历的时候都走在矩阵内,但是只有在范围内的坐标才会被记录
+    public int[][] spiralMatrixIII(int R, int C, int r0, int c0) {
+        //其实就是一个走迷宫问题,走过的地方变成墙,直到遍历完整个矩阵
+        int[][] ans = new int[R * C][2];
+
+        //补全矩阵
+        int maxRow = Math.max(r0, R - 1 - r0);
+        int maxCol =  Math.max(c0, C - 1 - c0);
+        int length = Math.max(maxRow, maxCol);
+        int[][] full = new int[length * 2 + 1][length * 2 + 1];
+        //补全后的矩阵的坐标以及起始点位置
+        int fullX = length;
+        int fullY = length;
+        for(int i = 0; i < R * C;){
+            //先向右走
+            while(true){
+                //首先将当前点置1,表示已经走过了
+                full[fullX][fullY] = 1;
+                //如果当前点在范围内,记录该点,i自加
+                if(   ((0 <= fullX - length && R - 1 - r0 >= fullX - length) ||
+                        (0 <= - fullX + length && r0 - 0 >= -fullX + length)) &&
+                        ((0 <= fullY - length && C - 1 - c0 >= fullY - length) ||
+                                (0 <= - fullY + length && c0 - 0 >= -fullY + length))){
+                    ans[i++] = new int[]{fullX - length + r0, fullY - length + c0};
+                }
+                //向右走一步
+                fullY++;
+                if(fullY == 2 * length + 1){//该情况表示已经走到最后了
+                    break;
+                }
+                //如果当前点可以往下走,那么就往下走,否则,继续向右走
+                if(full[fullX + 1][fullY] == 0){//表示该点未走过
+                    //表示下一个点可以向下走了,退出,进行向下走的循环
+                    break;
+                }
+
+            }
+
+            //向下走,如果向下走是1,直接break;进行这个判断是为了防止已经走到最后跳出的情况
+            while(true){
+                if(fullY == 2 * length + 1){
+                    break;
+                }
+                //首先将当前点置1,表示已经走过了
+                full[fullX][fullY] = 1;
+                //如果当前点在范围内,记录该点,i自加
+                if(   ((0 <= fullX - length && R - 1 - r0 >= fullX - length) ||
+                        (0 <= - fullX + length && r0 - 0 >= -fullX + length)) &&
+                        ((0 <= fullY - length && C - 1 - c0 >= fullY - length) ||
+                                (0 <= - fullY + length && c0 - 0 >= -fullY + length))){
+                    ans[i++] = new int[]{fullX - length + r0, fullY - length + c0};
+                }
+                //向下走一步
+                fullX++;
+                //如果当前点可以往左走,那么就往左走,否则,继续向下走
+                if(full[fullX][fullY - 1] == 0){//表示该点未走过
+                    //表示下一个点可以向左走了,退出,进行向左走的循环
+                    break;
+                }
+
+            }
+            //向左走,如果向左走是1,直接break;
+            while(true){
+                if(fullY == 2 * length + 1){
+                    break;
+                }
+                //首先将当前点置1,表示已经走过了
+                full[fullX][fullY] = 1;
+                //如果当前点在范围内,记录该点,i自加
+                if(   ((0 <= fullX - length && R - 1 - r0 >= fullX - length) ||
+                        (0 <= - fullX + length && r0 - 0 >= -fullX + length)) &&
+                        ((0 <= fullY - length && C - 1 - c0 >= fullY - length) ||
+                                (0 <= - fullY + length && c0 - 0 >= -fullY + length))){
+                    ans[i++] = new int[]{fullX - length + r0, fullY - length + c0};
+                }
+                //向下走一步
+                fullY--;
+                //如果当前点可以往上走,那么就往上走,否则,继续向左走
+                if(full[fullX - 1][fullY] == 0){//表示该点未走过
+                    //表示下一个点可以向左走了,退出,进行向左走的循环
+                    break;
+                }
+
+            }
+            //向上走,如果向上走是1,直接break;
+            while(true){
+                if(fullY == 2 * length + 1){
+                    break;
+                }
+                //首先将当前点置1,表示已经走过了
+                full[fullX][fullY] = 1;
+                //如果当前点在范围内,记录该点,i自加
+                if(   ((0 <= fullX - length && R - 1 - r0 >= fullX - length) ||
+                        (0 <= - fullX + length && r0 - 0 >= -fullX + length)) &&
+                        ((0 <= fullY - length && C - 1 - c0 >= fullY - length) ||
+                                (0 <= - fullY + length && c0 - 0 >= -fullY + length))){
+                    ans[i++] = new int[]{fullX - length + r0, fullY - length + c0};
+                }
+                //向上走一步
+                fullX--;
+                //如果当前点可以往上走,那么就往上走,否则,继续向左走
+                if(full[fullX][fullY + 1] == 0){//表示该点未走过
+                    //表示下一个点可以向左走了,退出,进行向左走的循环
+                    break;
+                }
+
+            }
+        }
+        return ans;
+    }
+}
