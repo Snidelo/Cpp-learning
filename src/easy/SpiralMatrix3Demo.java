66469a89f0dbf1d22c883647d5e23ce1dfb6cc66
Snidelo_T490 <huazefeng95@gmail.com>
2019-10-02 21:23:24 +0800

    new solution for Spiral Matrix, need to be reconsidered

--- src/easy/SpiralMatrix3Demo.java
+++ src/easy/SpiralMatrix3Demo.java
@@ -141,4 +141,33 @@ public class SpiralMatrix3Demo {
         }
         return ans;
     }
+
+    //========solution_3=========
+    //TODO:需要重新考虑一下这个算法,没怎么看懂
+    //第一次看应该是,每次走都会是固定的步数,比如第一次向右走走一步,第二次向右走要走四步,同理其他方向的步数
+    public int[][] spiralMatrixIII_3(int R, int C, int r0, int c0) {
+        int[] dr = new int[]{0, 1, 0, -1};
+        int[] dc = new int[]{1, 0, -1, 0};
+
+        int[][] ans = new int[R*C][2];
+        int t = 0;
+
+        ans[t++] = new int[]{r0, c0};
+        if (R * C == 1) return ans;
+
+        for (int k = 1; k < 2*(R+C); k += 2)
+            for (int i = 0; i < 4; ++i) {  // i: direction index
+                int dk = k + (i / 2);  // number of steps in this direction
+                for (int j = 0; j < dk; ++j) {  // for each step in this direction...
+                    // step in the i-th direction
+                    r0 += dr[i];
+                    c0 += dc[i];
+                    if (0 <= r0 && r0 < R && 0 <= c0 && c0 < C) {
+                        ans[t++] = new int[]{r0, c0};
+                        if (t == R * C) return ans;
+                    }
+                }
+            }
+        throw null;
+    }
 }
